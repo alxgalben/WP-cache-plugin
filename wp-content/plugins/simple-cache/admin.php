7cec6847edd6efc39f4cad025fc6c3f2aa1e0479
alxgalben <galbenalex2@gmail.com>
2024-12-20 02:49:32 +0200

    update

--- wp-content/plugins/simple-cache/admin.php
+++ wp-content/plugins/simple-cache/admin.php
@@ -13,13 +13,21 @@ function sc_add_admin_menu() {
 
 function sc_admin_page() {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Salvarea setărilor
         update_option('sc_cache_enabled', isset($_POST['cache_enabled']) ? 1 : 0);
-        update_option('sc_cache_exceptions', array_map('sanitize_text_field', $_POST['exceptions']));
-        update_option('sc_cache_durations', sc_parse_durations($_POST['durations']));
-    }
+
+// Gestionarea excepțiilor
+if (isset($_POST['exceptions'])) {
+    $exceptions = array_map('sanitize_text_field', explode("\n", trim($_POST['exceptions'])));
+    update_option('sc_cache_exceptions', $exceptions);
+}
+
+if (isset($_POST['durations'])) {
+    update_option('sc_cache_durations', sc_parse_durations($_POST['durations']));
+}
 
     $enabled = get_option('sc_cache_enabled');
-    $exceptions = get_option('sc_cache_exceptions', []);
+    $exceptions = implode("\n", get_option('sc_cache_exceptions', []));
     $durations = sc_format_durations(get_option('sc_cache_durations', []));
     ?>
     <div class="wrap">
@@ -30,21 +38,58 @@ function sc_admin_page() {
                 Enable Cache
             </label>
             <br><br>
-            <label>
-                Exceptions (one per line):
-                <textarea name="exceptions" rows="5" cols="50"><?php echo esc_textarea(implode("\n", $exceptions)); ?></textarea>
-            </label>
+            <label>Exceptions:</label>
+            <div id="exception-list">
+                <ul>
+                    <?php
+                    $saved_exceptions = get_option('sc_cache_exceptions', []);
+                    foreach ($saved_exceptions as $exception) {
+                        echo "<li>$exception <button type='button' class='remove-exception'>Remove</button></li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+            <input type="text" id="new-exception" placeholder="Add new exception">
+            <button type="button" id="add-exception" class="button">Add Exception</button>
             <br><br>
-            <label>
-                Cache Durations (e.g., checkout: 0):
-                <input type="text" name="durations" value="<?php echo esc_attr($durations); ?>">
-            </label>
+            <label>Cache Durations:</label>
+            <input type="text" name="durations" value="<?php echo esc_attr($durations); ?>">
             <br><br>
             <button type="submit" class="button button-primary">Save Settings</button>
         </form>
     </div>
-    <?php
-}
+
+    <script>
+        document.getElementById('add-exception').addEventListener('click', function() {
+            var newException = document.getElementById('new-exception').value.trim();
+            if (newException) {
+                var ul = document.querySelector('#exception-list ul');
+                var li = document.createElement('li');
+                li.innerHTML = newException + ' <button type="button" class="remove-exception">Remove</button>';
+                ul.appendChild(li);
+                document.getElementById('new-exception').value = ''; // Reset the input
+            }
+        });
+
+        // Remove exception when clicking 'Remove'
+        document.querySelector('#exception-list').addEventListener('click', function(event) {
+            if (event.target.classList.contains('remove-exception')) {
+                var li = event.target.closest('li');
+                li.parentNode.removeChild(li);
+            }
+        });
+
+        // Before submitting form, collect exceptions
+        document.querySelector('form').addEventListener('submit', function() {
+            var exceptions = [];
+            var exceptionItems = document.querySelectorAll('#exception-list ul li');
+            exceptionItems.forEach(function(item) {
+                exceptions.push(item.firstChild.textContent.trim());
+            });
+            document.querySelector('textarea[name="exceptions"]').value = exceptions.join("\n");
+        });
+    </script>
+<?php }
 
 function sc_parse_durations($input) {
     $durations = [];
@@ -63,3 +108,4 @@ function sc_format_durations($durations) {
     }
     return implode(',', $formatted);
 }
+?>
